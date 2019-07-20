5bc5ffe1b8a14ea6dabf66c2e3cb6c27d7009ee4
Shobhit Chittora <chittorashobhit@gmail.com>
2019-07-20 18:13:27 +0530

    change app name in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-fashion-portfolio`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
