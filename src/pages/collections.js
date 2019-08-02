8f146a555c5d1023765490f1ae3508b8366f7a6c
Shobhit Chittora <schittora@paypal.com>
2019-08-02 19:49:34 +0530

    fixing formatting issues

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Layout from '@components/Layout'
-import SEO from '@components/SEO'
-import CollectionPageContainer from '@components/CollectionPage'
+import React from "react"
+import Layout from "@components/Layout"
+import SEO from "@components/SEO"
+import CollectionPageContainer from "@components/CollectionPage"
 
 function CollectionPage() {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from "react"
 import Layout from "@components/Layout"
 import SEO from "@components/SEO"
-import HomePage from '@components/HomePage'
+import HomePage from "@components/HomePage"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomePage />
-    
   </Layout>
 )
 
